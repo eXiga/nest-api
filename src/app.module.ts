3d37e0ce9bca4b00ce8156f4b0cafbd3da90ae63
eXiga <anton.kostenich@hotmail.com>
2023-01-30 18:50:17 +0100

    add threats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { CategoriesModule } from './categories/categories.module';
 import { XRequestIdGuard } from './common/guards/xrid.guard';
+import { ThreatsModule } from './threats/threats.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, ThreatsModule],
   providers: [{ provide: APP_GUARD, useClass: XRequestIdGuard }],
 })
 export class AppModule {}
